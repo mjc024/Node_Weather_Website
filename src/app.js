e79553501eea60b279b7ab88d87ab8bf905c14cc
MJC <mujtabajawed20@gmail.com>
2022-10-19 17:21:27 +0500

    Init Commit

---
+++ src/app.js
@@ -0,0 +1,59 @@
+const path =require('path')
+const express= require('express')
+
+
+// console.log(__dirname)
+// console.log(path.join(__dirname,'../public'))
+
+const publicDirectory=path.join(__dirname,'../public')
+
+const app=express()
+app.set('view engine','hbs')
+app.use(express.static(publicDirectory))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: 'Weather App',
+        name1: 'mjc'
+    })
+})
+
+app.get('/about',(req,res)=>{
+     res.render('about',{
+        title:"About me ",
+        name1: "mjc"
+     })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        msg: "This page is to help you"
+    })
+})
+// app.get('',(req,res)=>{
+//     res.send('<h1>Weather</h1> <body><p>hellow stiup sdnjn</p></body>'+{
+//         name1:'andrew',
+//         age:23
+//     })
+// })
+
+    // app.get('/help',(req,res)=>{
+    //     res.send({
+    //         name1:'andrew',
+    //         age:23
+    //     })
+    // })
+
+    // app.get('/about',(req,res)=>{
+    //     res.send('<h1>About page</h1>')
+    // })
+app.get('/weather',(req,res)=>{
+    res.send({
+        location:'Pakistan',
+        forecast: 'Sunny'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("Server is up on port 3000")
+})
